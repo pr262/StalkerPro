166a4577dcf1c34eeaf336daee9d47df29f75d74
wdani <wdanielsson70@gmail.com>
2025-04-08 12:58:09 +0200

    Added progress bar, log clearing and threading

--- StalkerPro/Form1.Designer.cs
+++ StalkerPro/Form1.Designer.cs
@@ -10,7 +10,7 @@
         /// <summary>
         ///  Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true om managed resources ska tas bort; annars false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,14 +23,12 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        ///  Metod som krävs för Designer support - ändra inte innehållet i denna metod med kodredigeraren.
         /// </summary>
         private void InitializeComponent()
         {
             txtFirstName = new TextBox();
             button1 = new Button();
-            lstResults = new ListBox();
             txtLog = new TextBox();
             txtLocation = new TextBox();
             numMaxAge = new NumericUpDown();
@@ -41,134 +39,158 @@
             label4 = new Label();
             label5 = new Label();
             txtLastName = new TextBox();
+            dgvPeople = new DataGridView();
+            progressBar = new ProgressBar();
             ((System.ComponentModel.ISupportInitialize)numMaxAge).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numMinAge).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvPeople).BeginInit();
             SuspendLayout();
             // 
             // txtFirstName
             // 
-            txtFirstName.Location = new Point(782, 11);
-            txtFirstName.Margin = new Padding(3, 2, 3, 2);
+            txtFirstName.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            txtFirstName.Location = new Point(894, 15);
             txtFirstName.Name = "txtFirstName";
-            txtFirstName.Size = new Size(216, 23);
+            txtFirstName.Size = new Size(246, 27);
             txtFirstName.TabIndex = 0;
             txtFirstName.Text = "William";
             // 
             // button1
             // 
-            button1.Location = new Point(976, 107);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button1.Location = new Point(1115, 143);
             button1.Name = "button1";
-            button1.Size = new Size(74, 21);
+            button1.Size = new Size(85, 28);
             button1.TabIndex = 1;
             button1.Text = "Sök";
             button1.UseVisualStyleBackColor = true;
             button1.Click += btnSearch_Click;
             // 
-            // lstResults
-            // 
-            lstResults.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-            lstResults.FormattingEnabled = true;
-            lstResults.ItemHeight = 15;
-            lstResults.Location = new Point(12, 8);
-            lstResults.Margin = new Padding(3, 2, 3, 2);
-            lstResults.Name = "lstResults";
-            lstResults.Size = new Size(673, 319);
-            lstResults.TabIndex = 2;
-            // 
             // txtLog
             // 
-            txtLog.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            txtLog.Location = new Point(714, 132);
-            txtLog.Margin = new Padding(3, 2, 3, 2);
+            txtLog.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            txtLog.Location = new Point(816, 176);
             txtLog.Multiline = true;
             txtLog.Name = "txtLog";
             txtLog.ScrollBars = ScrollBars.Both;
-            txtLog.Size = new Size(336, 198);
+            txtLog.Size = new Size(383, 237);
             txtLog.TabIndex = 3;
             // 
             // txtLocation
             // 
-            txtLocation.Location = new Point(752, 66);
+            txtLocation.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            txtLocation.Location = new Point(859, 88);
+            txtLocation.Margin = new Padding(3, 4, 3, 4);
             txtLocation.Name = "txtLocation";
-            txtLocation.Size = new Size(246, 23);
+            txtLocation.Size = new Size(281, 27);
             txtLocation.TabIndex = 4;
             txtLocation.Text = "Helsingborg";
             // 
             // numMaxAge
             // 
-            numMaxAge.Location = new Point(752, 95);
+            numMaxAge.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            numMaxAge.Location = new Point(859, 127);
+            numMaxAge.Margin = new Padding(3, 4, 3, 4);
             numMaxAge.Name = "numMaxAge";
-            numMaxAge.Size = new Size(59, 23);
+            numMaxAge.Size = new Size(67, 27);
             numMaxAge.TabIndex = 5;
+            numMaxAge.Value = new decimal(new int[] { 25, 0, 0, 0 });
             // 
             // numMinAge
             // 
-            numMinAge.Location = new Point(875, 95);
+            numMinAge.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            numMinAge.Location = new Point(1001, 127);
+            numMinAge.Margin = new Padding(3, 4, 3, 4);
             numMinAge.Name = "numMinAge";
-            numMinAge.Size = new Size(59, 23);
+            numMinAge.Size = new Size(67, 27);
             numMinAge.TabIndex = 6;
-            numMinAge.Value = new decimal(new int[] { 25, 0, 0, 0 });
+            numMinAge.Value = new decimal(new int[] { 15, 0, 0, 0 });
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label1.AutoSize = true;
-            label1.Location = new Point(714, 97);
+            label1.Location = new Point(816, 129);
             label1.Name = "label1";
-            label1.Size = new Size(32, 15);
+            label1.Size = new Size(40, 20);
             label1.TabIndex = 7;
             label1.Text = "Max:";
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label2.AutoSize = true;
-            label2.Location = new Point(838, 97);
+            label2.Location = new Point(958, 129);
             label2.Name = "label2";
-            label2.Size = new Size(31, 15);
+            label2.Size = new Size(37, 20);
             label2.TabIndex = 8;
             label2.Text = "Min:";
             // 
             // label3
             // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label3.AutoSize = true;
-            label3.Location = new Point(714, 69);
+            label3.Location = new Point(816, 92);
             label3.Name = "label3";
-            label3.Size = new Size(31, 15);
+            label3.Size = new Size(37, 20);
             label3.TabIndex = 9;
             label3.Text = "City:";
             // 
             // label4
             // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label4.AutoSize = true;
-            label4.Location = new Point(714, 14);
+            label4.Location = new Point(816, 19);
             label4.Name = "label4";
-            label4.Size = new Size(67, 15);
+            label4.Size = new Size(83, 20);
             label4.TabIndex = 10;
             label4.Text = "First Name:";
             // 
             // label5
             // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label5.AutoSize = true;
-            label5.Location = new Point(714, 41);
+            label5.Location = new Point(816, 55);
             label5.Name = "label5";
-            label5.Size = new Size(64, 15);
+            label5.Size = new Size(79, 20);
             label5.TabIndex = 12;
             label5.Text = "Last name:";
             // 
             // txtLastName
             // 
-            txtLastName.Location = new Point(782, 38);
-            txtLastName.Margin = new Padding(3, 2, 3, 2);
+            txtLastName.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            txtLastName.Location = new Point(894, 51);
             txtLastName.Name = "txtLastName";
-            txtLastName.Size = new Size(216, 23);
+            txtLastName.Size = new Size(246, 27);
             txtLastName.TabIndex = 11;
             txtLastName.Text = "Danielsson";
             // 
+            // dgvPeople
+            // 
+            dgvPeople.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvPeople.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvPeople.Location = new Point(10, 10);
+            dgvPeople.Margin = new Padding(2);
+            dgvPeople.Name = "dgvPeople";
+            dgvPeople.RowHeadersWidth = 62;
+            dgvPeople.Size = new Size(786, 403);
+            dgvPeople.TabIndex = 13;
+            // 
+            // progressBar
+            // 
+            progressBar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            progressBar.Location = new Point(10, 418);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(1190, 29);
+            progressBar.TabIndex = 14;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1062, 338);
+            ClientSize = new Size(1214, 451);
+            Controls.Add(progressBar);
+            Controls.Add(dgvPeople);
             Controls.Add(label5);
             Controls.Add(txtLastName);
             Controls.Add(label4);
@@ -179,32 +201,32 @@
             Controls.Add(numMaxAge);
             Controls.Add(txtLocation);
             Controls.Add(txtLog);
-            Controls.Add(lstResults);
             Controls.Add(button1);
             Controls.Add(txtFirstName);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
-            Text = "Form1";
+            Text = "StalkerPro";
             ((System.ComponentModel.ISupportInitialize)numMaxAge).EndInit();
             ((System.ComponentModel.ISupportInitialize)numMinAge).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvPeople).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private TextBox txtFirstName;
-        private Button button1;
-        private ListBox lstResults;
-        private TextBox txtLog;
-        private TextBox txtLocation;
-        private NumericUpDown numMaxAge;
-        private NumericUpDown numMinAge;
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
-        private TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.NumericUpDown numMaxAge;
+        private System.Windows.Forms.NumericUpDown numMinAge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.DataGridView dgvPeople;
+        private ProgressBar progressBar;
     }
 }
--- StalkerPro/Form1.cs
+++ StalkerPro/Form1.cs
@@ -1,10 +1,11 @@
 using System;
-using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using HtmlAgilityPack;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 
 namespace StalkerPro
@@ -14,113 +15,231 @@ namespace StalkerPro
         public Form1()
         {
             InitializeComponent();
+            // Set up DataGridView columns.
+            dgvPeople.Columns.Clear();
+            dgvPeople.Columns.Add("Personnamn", "Personnamn");
+            dgvPeople.Columns.Add("Tilltalsnamn", "Tilltalsnamn");
+            dgvPeople.Columns.Add("Efternamn", "Efternamn");
+            dgvPeople.Columns.Add("Personnummer", "Personnummer");
+            dgvPeople.Columns.Add("URL", "Profil Länk");
+
+            // Initialize the progress bar.
+            progressBar.Visible = false;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+            progressBar.Value = 0;
         }
 
+        // Thread-safe Debug method.
         public void Debug(string message)
         {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => Debug(message)));
+                return;
+            }
             txtLog.AppendText(message + Environment.NewLine);
         }
 
         private async void btnSearch_Click(object sender, EventArgs e)
         {
+            txtLog.Clear();
             string firstName = txtFirstName.Text.Trim();
             string lastName = txtLastName.Text.Trim();
             string location = txtLocation.Text.Trim();
             int minAge = (int)numMinAge.Value;
             int maxAge = (int)numMaxAge.Value;
 
-            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+            if (string.IsNullOrWhiteSpace(firstName))
             {
-                Debug("Förnamn och efternamn är obligatoriska.");
+                Debug("Förnamn är obligatoriskt.");
                 return;
             }
+
+            progressBar.Visible = true;
+            progressBar.Value = 0;
 
             try
             {
                 var options = new ChromeOptions();
-                options.AddArgument("--headless"); // Kör utan GUI
+                options.AddArgument("--headless");
                 options.AddArgument("--disable-gpu");
                 options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
 
-                using (var driver = new ChromeDriver(options))
+                var service = ChromeDriverService.CreateDefaultService();
+                service.HideCommandPromptWindow = true;
+
+                using (var driver = new ChromeDriver(service, options))
                 {
-                    // Build the URL dynamically, including empty parameters
+                    // STEP 1: Navigate to search URL.
                     string searchUrl = $"https://www.ratsit.se/sok/person?" +
                                        $"fnamn={Uri.EscapeDataString(firstName)}" +
                                        $"&enamn={Uri.EscapeDataString(lastName)}" +
-                                       $"&gata=" + // Empty parameter
-                                       $"&postnr=" + // Empty parameter
-                                       $"&ort=" + // Empty parameter
                                        $"&kn={Uri.EscapeDataString(location)}" +
-                                       $"&pnr=" + // Empty parameter
-                                       $"&tfn=" + // Empty parameter
-                                       $"&m=0" +  // Default parameter
-                                       $"&k=0" +  // Default parameter
-                                       $"&r=0" +  // Default parameter
-                                       $"&er=0" + // Default parameter
-                                       $"&b=0" +  // Default parameter
-                                       $"&eb=0" + // Default parameter
                                        $"&amin={minAge}" +
                                        $"&amax={maxAge}" +
-                                       $"&fon=1" + // Default parameter
-                                       $"&page=1"; // Default pagination
+                                       "&fon=1&page=1";
 
                     Debug($"Söker på URL: {searchUrl}");
                     driver.Navigate().GoToUrl(searchUrl);
+                    await Task.Delay(3000);  // Wait for page load.
+                    progressBar.Value = 10; // Update to 10% after navigation.
 
-                    // Hitta och klicka på knappen för att godkänna cookies
-                    try
-                    {
-                        var acceptButton = driver.FindElement(By.Id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"));
-                        acceptButton.Click();
-                        Debug("Godkände cookies.");
-                    }
-                    catch (NoSuchElementException)
-                    {
-                        Debug("Hittade ingen Cookie Consent-knapp. Fortsätter ändå...");
-                    }
-
-                    // Vänta tills sidan laddas
-                    System.Threading.Thread.Sleep(2000);
-
-                    // Hämta sidans HTML
+                    // STEP 2: Retrieve and parse HTML.
                     string htmlContent = driver.PageSource;
-                    Debug("Lyckades hämta HTML efter cookie-godkännande.");
-
-                    // Bearbeta HTML med HtmlAgilityPack
+                    Debug("Lyckades hämta HTML.");
                     HtmlDocument htmlDoc = new HtmlDocument();
                     htmlDoc.LoadHtml(htmlContent);
+                    progressBar.Value = 20; // Update to 20% after parsing HTML.
 
-                    // Hämta länkar till personprofiler
                     var profileLinks = htmlDoc.DocumentNode.SelectNodes("//ul[contains(@class, 'search-result-list')]//li//a[@href]");
                     if (profileLinks == null || profileLinks.Count == 0)
                     {
-                        Debug($"Inga relevanta länkar hittades för \"{firstName} {lastName}\".");
+                        Debug($"Inga resultat för \"{firstName} {lastName}\".");
+                        progressBar.Value = 100;
                         return;
                     }
+                    dgvPeople.Rows.Clear();
 
-                    lstResults.Items.Clear();
+                    // STEP 3: Process profile links.
+                    int totalLinks = profileLinks.Count;
+                    // Allocate progress from 20% (done with initialization) to 90% for processing all links.
+                    double progressPerLink = totalLinks > 0 ? (70.0 / totalLinks) : 0;
+                    int linkIndex = 0;
+
                     foreach (var link in profileLinks)
                     {
-                        string href = link.Attributes["href"].Value;
+                        PersonDetails details = await Task.Run(() => ProcessProfileLink(link, minAge, maxAge));
+                        linkIndex++;
+                        // Calculate new progress value.
+                        int progressValue = 20 + (int)(progressPerLink * linkIndex);
+                        if (progressValue > 90)
+                            progressValue = 90;
+                        progressBar.Value = progressValue;
 
-                        // Kontrollera om länken är absolut eller relativ
-                        if (!href.StartsWith("http"))
+                        if (details != null)
                         {
-                            href = "https://www.ratsit.se" + href;
+                            // Use Invoke to safely update the DataGridView on the UI thread.
+                            this.Invoke(new Action(() =>
+                            {
+                                dgvPeople.Rows.Add(details.Personnamn, details.Tilltalsnamn, details.Efternamn, details.Personnummer, details.Url);
+                            }));
+                            Debug($"Lägger till i DataGridView: {details.Personnamn}, {details.Tilltalsnamn}, {details.Efternamn}, {details.Personnummer}, {details.Url}");
                         }
-
-                        lstResults.Items.Add(href);
-                        Debug($"Hittade personlänk: {href}");
+                        await Task.Yield(); // Let the UI update.
                     }
+                    // STEP 4: Finalizing.
+                    progressBar.Value = 100;
+                    await Task.Delay(500);
+                    progressBar.Visible = false;
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Ett fel inträffade: {ex.Message}", "Fel", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Debug($"Ett fel inträffade: {ex.Message}");
+                MessageBox.Show($"Fel: {ex.Message}", "Fel", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Debug($"Fel: {ex.Message}");
             }
         }
 
+        /// <summary>
+        /// Processes a profile link on a background thread.
+        /// </summary>
+        private PersonDetails ProcessProfileLink(HtmlAgilityPack.HtmlNode link, int minAge, int maxAge)
+        {
+            try
+            {
+                string href = link.Attributes["href"].Value;
+                if (!href.StartsWith("http"))
+                    href = "https://www.ratsit.se" + href;
+                if (href.Contains("bolagsfakta.se") || href.Contains("ratsit.se/kop/kassa"))
+                    return null;
+
+                // Extract the personal number (e.g., 20071012) from the URL.
+                var match = Regex.Match(href, @"ratsit\.se\/(\d{8})-");
+                if (!match.Success) return null;
+                string personalNumber = match.Groups[1].Value;
+                int age = GetAge(personalNumber);
+                Debug($"Extraherade personnummer: {personalNumber}, Ålder: {age}");
+                if (age < minAge || age > maxAge)
+                    return null;
+
+                PersonDetails details = ScrapePersonDetails(href);
+                if (details != null)
+                    details.Personnummer = personalNumber;
+
+                return details;
+            }
+            catch (Exception ex)
+            {
+                Debug("Fel vid bearbetning av länk: " + ex.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Scrapes person details from the given URL using Selenium.
+        /// </summary>
+        private PersonDetails ScrapePersonDetails(string url)
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+
+            var service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+
+            using (var driver = new ChromeDriver(service, options))
+            {
+                driver.Navigate().GoToUrl(url);
+                System.Threading.Thread.Sleep(3000);  // Wait for page load.
+                string html = driver.PageSource;
+
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(html);
+                Debug("HTML-snutt: " + (html.Length > 300 ? html.Substring(0, 300) : html));
+
+                string personnamn = ExtractValue(doc, "//p[span[contains(text(),'Personnamn:')]]", "Personnamn:");
+                string tilltalsnamn = ExtractValue(doc, "//p[span[contains(text(),'Tilltalsnamn:')]]", "Tilltalsnamn:");
+                string efternamn = ExtractValue(doc, "//p[span[contains(text(),'Efternamn:')]]", "Efternamn:");
+
+                return new PersonDetails
+                {
+                    Personnamn = personnamn,
+                    Tilltalsnamn = tilltalsnamn,
+                    Efternamn = efternamn,
+                    Url = url
+                };
+            }
+        }
+
+        private string ExtractValue(HtmlDocument doc, string xPath, string labelToRemove)
+        {
+            var node = doc.DocumentNode.SelectSingleNode(xPath);
+            return node == null ? "" : node.InnerText.Replace(labelToRemove, "").Trim();
+        }
+
+        private int GetAge(string personalNumber)
+        {
+            if (personalNumber.Length < 8)
+                return 0;
+            int year = int.Parse(personalNumber.Substring(0, 4));
+            int month = int.Parse(personalNumber.Substring(4, 2));
+            int day = int.Parse(personalNumber.Substring(6, 2));
+            DateTime birthDate = new DateTime(year, month, day);
+            int age = DateTime.Today.Year - birthDate.Year;
+            if (birthDate > DateTime.Today.AddYears(-age))
+                age--;
+            return age;
+        }
+    }
+
+    public class PersonDetails
+    {
+        public string Personnamn { get; set; }
+        public string Tilltalsnamn { get; set; }
+        public string Efternamn { get; set; }
+        public string Url { get; set; }
+        public string Personnummer { get; set; }
     }
 }
